b14424b41fbe5f528c783253f8f03032d460e0ed
Utkrist Karky <63005844+utkrist-karky@users.noreply.github.com>
2022-04-19 08:17:01 +0200

    init

---
+++ landing_page/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Next Step Tech</h1>
+        <a>
+          Coming Soon
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
